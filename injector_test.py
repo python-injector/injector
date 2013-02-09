19e2dd9ad4489d3e8bfff9f1d20cea796302764a
Jakub Stasiak <jakub@stasiak.at>
2013-02-09 02:57:00 +0000

    Extend member injection API and provide an implementation

--- injector_test.py
+++ injector_test.py
@@ -751,6 +751,11 @@ class TestMemberInjection(object):
         a = b.a
         assert (type(a) == self.A)
 
+    def test_member_pretends_to_not_exist_when_instance_not_created_by_injector(self):
+        b = self.B()
+        with pytest.raises(AttributeError):
+            getattr(b, 'a')
+
     def test_members_arent_injected_until_accessed(self):
         b = self.injector.get(self.B)
         assert (self.A.counter == 0)
@@ -759,11 +764,27 @@ class TestMemberInjection(object):
         b = self.injector.get(self.B)
         _1 = b.a
         _2 = b.a
-        assert (self.A.counter == 1 and _1 == _2)
+        assert (self.A.counter == 1 and _1 is _2)
 
-    def test_members_arent_injected_when_manually_overridden(self):
+    def test_each_instance_get_new_injection(self):
+        count = 3
+        for i in range(count):
+            b = self.injector.get(self.B)
+            a = b.a
+
+        assert (self.A.counter == count)
+
+    def test_members_arent_injected_when_manually_overwritten(self):
         b = self.injector.get(self.B)
-        b.a = 123
+        b.a = 'irrelevant'
         a = b.a
 
-        assert (self.A.counter == 0 and a == 123)
+        assert (self.A.counter == 0)
+
+    def test_members_can_be_overwritten(self):
+        for obj in (self.B(), self.injector.get(self.B)):
+            self._overwrite_member(obj)
+
+    def _overwrite_member(self, obj):
+        obj.a = 123
+        assert (obj.a == 123)
