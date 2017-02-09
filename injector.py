c7439497dc19ff13c3b6450998bd5f34983f1394
Jakub Stasiak <jakub@stasiak.at>
2017-02-09 13:55:41 +0100

    Prepare a new release (0.12.0)

--- injector.py
+++ injector.py
@@ -37,7 +37,7 @@ except AttributeError:
             pass
 
 __author__ = 'Alec Thomas <alec@swapoff.org>'
-__version__ = '0.11.1'
+__version__ = '0.12.0'
 __version_tag__ = ''
 
 log = logging.getLogger('injector')
