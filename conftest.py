ae39af0ad86e40aca449b88f75eb64940fb3cb87
Jakub Stasiak <jakub@stasiak.at>
2019-12-10 00:39:30 +0100

    Simplify pytest configuration
    It seems to be doing fine on its own.
    
    As a bonus this restores running doctests from the injector source code
    which I broke when I made it a package (code in injector/__init__.py)
    instead of a simple module. *But* we need to start skipping running
    doctests for get_bindings on Python versions that don't support
    Annotated (or don't support it well enough).

--- conftest.py
+++
@@ -1,7 +0,0 @@
-import os.path
-
-test_sources = ['injector', 'injector_test.py', 'README.md']
-
-
-def pytest_ignore_collect(path, config):
-    return not os.path.basename(str(path)) in test_sources
